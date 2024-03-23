68374f33660025da0ad3cac048e04f80685c5f9e
someonesocial <max135mustermann@gmail.com>
2024-03-23 17:53:47 +0100

    added color example and example code

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const beispiel1 = document.querySelector("#beispiel1");
 beispiel1.addEventListener("click", executeExample);
 
+// Prüfen ob sessionStorage.color existiert
+if ("color" in sessionStorage) {
+  document.querySelector("#sessionColor").value = sessionStorage.color;
+}
+
+// sessionStorage speichern
 function executeExample() {
-  alert("das ist ein Beispiel für den Funtionsaufruf");
+  sessionStorage.setItem(
+    "color",
+    document.querySelector("#sessionColor").value
+  );
 }
